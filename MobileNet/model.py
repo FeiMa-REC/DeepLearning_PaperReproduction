fda482d60dc288f4a9463573896b12ed4434a49f
FeiMa-REC <Mafei.07@foxmail.com>
2023-08-07 22:23:05 +0800

    update MobileNetV2 : model.py、predict.py、train.py

--- MobileNet/model.py
+++ MobileNet/model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torchsummary import summary
 
 
 def _make_divisible(ch, divisor=8, min_ch=None):
@@ -18,11 +19,11 @@ def _make_divisible(ch, divisor=8, min_ch=None):
     return new_ch
 
 
-class ConvBnReLU(nn.Sequential):
+class ConvBNReLU(nn.Sequential):
     def __init__(self, in_channel, out_channel, kernel_size=3, stride=1, groups=1):
         padding = (kernel_size - 1) // 2
-        super(ConvBnReLU, self).__init__(
-            nn.Conv2d(in_channel, out_channel, kernel_size, stride=stride, padding=padding, groups=groups, bias=False),
+        super(ConvBNReLU, self).__init__(
+            nn.Conv2d(in_channel, out_channel, kernel_size, stride, padding, groups=groups, bias=False),
             nn.BatchNorm2d(out_channel),
             nn.ReLU6(inplace=True)
         )
@@ -31,33 +32,32 @@ class ConvBnReLU(nn.Sequential):
 class InvertedResidual(nn.Module):
     def __init__(self, in_channel, out_channel, stride, expand_ratio):
         super(InvertedResidual, self).__init__()
-        # expand channel:即倒残差结构第一层卷积的out_channel
         hidden_channel = in_channel * expand_ratio
-        self.use_shotcut = stride == 1 and in_channel == out_channel
+        self.use_shortcut = stride == 1 and in_channel == out_channel
 
         layers = []
         if expand_ratio != 1:
-            # 1x1 Pointwise conv
-            layers.append(ConvBnReLU(in_channel, hidden_channel, kernel_size=1))
+            # 1x1 pointwise conv
+            layers.append(ConvBNReLU(in_channel, hidden_channel, kernel_size=1))
         layers.extend([
             # 3x3 depthwise conv
-            ConvBnReLU(hidden_channel, hidden_channel, kernel_size=3, stride=stride, groups=hidden_channel),
-            # 1x1 Pointwise conv & Linear 激活函数的输出等于输入，此处可以省略这一层
+            ConvBNReLU(hidden_channel, hidden_channel, stride=stride, groups=hidden_channel),
+            # 1x1 pointwise conv & Linear 激活函数的输出等于输入，此处可以省略这一层
             nn.Conv2d(hidden_channel, out_channel, kernel_size=1, bias=False),
-            nn.BatchNorm2d(out_channel)
+            nn.BatchNorm2d(out_channel),
         ])
 
-        self.InverResBlock = nn.Sequential(*layers)
+        self.conv = nn.Sequential(*layers)
 
     def forward(self, x):
-        if self.use_shotcut:
-            return x + self.InverResBlock(x)
+        if self.use_shortcut:
+            return x + self.conv(x)
         else:
-            return self.InverResBlock(x)
+            return self.conv(x)
 
 
 class MobileNetV2(nn.Module):
-    def __init__(self, num_classes=1000, alpha=1.0, round_nearest=8, drop_p=0.2):
+    def __init__(self, num_classes=1000, alpha=1.0, round_nearest=8):
         super(MobileNetV2, self).__init__()
         block = InvertedResidual
         input_channel = _make_divisible(32 * alpha, round_nearest)
@@ -75,23 +75,24 @@ class MobileNetV2(nn.Module):
         ]
 
         features = []
-        # first conv layer
-        features.append(ConvBnReLU(3, input_channel, stride=2))
-        # bottlenecks
+        # conv1 layer
+        features.append(ConvBNReLU(3, input_channel, stride=2))
+        # building inverted residual residual blockes
         for t, c, n, s in inverted_residual_setting:
             output_channel = _make_divisible(c * alpha, round_nearest)
             for i in range(n):
                 stride = s if i == 0 else 1
                 features.append(block(input_channel, output_channel, stride, expand_ratio=t))
                 input_channel = output_channel
-        # conv after bottlenecks
-        features.append(ConvBnReLU(input_channel, last_channel, kernel_size=1))
+        # building last several layers
+        features.append(ConvBNReLU(input_channel, last_channel, 1))
+        # combine feature layers
         self.features = nn.Sequential(*features)
 
-        # classifier
+        # building classifier
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.classifier = nn.Sequential(
-            nn.Dropout(drop_p),
+            nn.Dropout(0.2),
             nn.Linear(last_channel, num_classes)
         )
 
@@ -119,3 +120,6 @@ class MobileNetV2(nn.Module):
 if __name__ == "__main__":
     net = MobileNetV2(num_classes=5)
     print(net)
+    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # net = MobileNetV2(num_classes=5).to(device=device)
+    # summary(net, (3, 224, 224))
---
+++ MobileNet/train.py
@@ -0,0 +1,143 @@
+import os
+import torch
+import json
+import torch.nn as nn
+from torchvision import datasets, transforms
+from torch.utils.tensorboard import SummaryWriter
+from model import MobileNetV2
+
+
+def main(batch_size=32, epochs=2):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("using {} for training.".format(device))
+    # 添加可视化
+    writer = SummaryWriter('./logs')
+
+    data_transform = {
+        "train": transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406],
+                                 [0.229, 0.224, 0.225])
+        ]),
+        "val": transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406],
+                                 [0.229, 0.224, 0.225])
+        ])
+    }
+
+    data_root = "D:/BaiduNetdiskDownload/DataSets/flower_data"
+    train_dataset = datasets.ImageFolder(os.path.join(data_root, "train"),
+                                         transform=data_transform["train"])
+    train_data_num = len(train_dataset)
+    val_dataset = datasets.ImageFolder(os.path.join(data_root, "val"),
+                                       transform=data_transform["val"])
+    val_data_num = len(val_dataset)
+    print("using {} images fot training, {} images for validation.".format(
+        train_data_num, val_data_num
+    ))
+
+    # {'daisy':0, 'dandelion':1, 'roses':2, 'sunflower':3, 'tulips':4}
+    flower_dict = train_dataset.class_to_idx
+    cla_dict = dict((val, key) for key, val in flower_dict.items())
+    # write dict to json file
+    json_str = json.dumps(cla_dict, indent=4)
+    with open('class_indices.json', 'w') as json_file:
+        json_file.write(json_str)
+
+    num_workers = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])
+    print("using {} dataloader workers every process.\n".format(num_workers))
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+                                               batch_size=batch_size,
+                                               shuffle=True,
+                                               num_workers=num_workers)
+    val_loader = torch.utils.data.DataLoader(val_dataset,
+                                             batch_size=batch_size,
+                                             shuffle=True,
+                                             num_workers=num_workers)
+
+    net = MobileNetV2(num_classes=5)
+    weight_pth = './ModelHub/mobilenet_v2.pth'
+    pre_weights = torch.load(weight_pth)
+    pre_dict = {k: v for k, v in pre_weights.items() if "classifier" not in k}
+    missing_keys, unexpected_keys = net.load_state_dict(pre_dict, strict=False)
+
+    # freeze features weights
+    for param in net.features.parameters():
+        param.requires_grad = False
+
+    net.to(device)
+
+    loss_function = nn.CrossEntropyLoss()
+    params = [p for p in net.parameters() if p.requires_grad]
+    optimizer = torch.optim.Adam(params, lr=0.0001)
+
+    best_acc = 0.0
+    train_steps = len(train_loader)
+
+    for epoch in range(epochs):
+        # train
+        net.train()
+        running_loss = 0.0
+        for step, data in enumerate(train_loader, start=0):
+            images, labels = data
+            optimizer.zero_grad()
+            logits = net(images.to(device))
+            loss = loss_function(logits, labels.to(device))
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            writer.add_scalar("TrainLoss", loss, step)
+
+            rate = (step+1)/len(train_loader)
+            a = "*" * int(rate*50)
+            b = "." * int((1-rate) * 50)
+            print("\rtrain_loss: {:^3.0f}%[{}->{}]{:.3f}".format(
+                int(rate*100), a, b, loss
+            ), end="")
+        print()
+        print("training processes finished.")
+
+        # val
+        net.eval()
+        acc = 0.0
+        with torch.no_grad():
+            for step, val_data in enumerate(val_loader, start=0):
+                val_imaged, val_labels = val_data
+                outputs = net(val_imaged.to(device))
+                predict_y = torch.max(outputs, dim=1)[1]
+                acc += torch.eq(predict_y, val_labels.to(device)).sum().item()
+
+                rate = (step + 1) / len(val_loader)
+                a = "*" * int(rate * 50)
+                b = "." * int((1 - rate) * 50)
+                print("\rAcc: {:^3.0f}%[{}->{}]{:.3f}".format(
+                    int(rate * 100), a, b, acc
+                ), end="")
+            print()
+            print("validation processes finished.")
+
+        val_accurate = acc / val_data_num
+        writer.add_scalar("val_acc", val_accurate, epoch+1)
+        print("[epoch {}]\ttrain_loss: {:.3f}\tval_accuracy: {:.3f}\n".format(
+            epoch+1, running_loss / train_steps, val_accurate
+        ))
+
+        if val_accurate > best_acc:
+            best_acc = val_accurate
+            torch.save(net.state_dict(), f="./checkpoints/Trained_MobileNetV2_{}.pth".format(epoch+1))
+            print("models had save in ./checkpoints/Trained_MobileNetV2_{}.pth".format(epoch + 1))
+
+    writer.close()
+    print("Finished Training.")
+
+
+if __name__ == "__main__":
+    batch_size = 32
+    epochs = 2
+    main(batch_size, epochs)
